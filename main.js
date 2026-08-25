24fed4a0e1b40b7239ce44043881d5088d052bf8
yogenkarki07 <yogeen399@gmail.com>
2026-08-25 12:42:33 +1200

    home page UI done. Other features WIP

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 
 const apiURL = "./dummy.json"; 
 
-async function getWeather() {
+async function Weather() {
     try{
 
     //fetch the json file
@@ -18,17 +18,14 @@ async function getWeather() {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    //convert json response into JavaScript object
+    //convert json/API response into JavaScript object
     const data = await response.json();
 
-    //get the required information
-    const location = data.location;
-    const current = data.current;
+    //data calling from API/json into HTML
+    updatingHomeUI(data);
 
-    //data called from json into HTML
-    updatebodysection();
-
-
+    //data calling from API into grid-boxes elements
+    homeGrid(data);
 
    }
 
@@ -38,8 +35,47 @@ async function getWeather() {
 
 }
 
-function updatebodysection(){
-    document.getElementById("city").textContent = `${location.name},${location.country}`;
+//home UI function
+function updatingHomeUI(data){
+    //get the required information from AIP/json
+    const location = data.location;
+    const current = data.current;
+
+    // window.mytime = location; <super class="super">&deg;c</super>
+
+    //creting variables -- for HTML elements
+    const cityElement = document.getElementById("city");
+    const timeElement = document.getElementById("time");
+    const iconElement = document.getElementById("icon");
+    const tempElement = document.getElementById("temperature");
+    const descripElement = document.getElementById("description");
+
+    //putting API data into HTML elements
+    cityElement.textContent = `${location.name}`;
+    timeElement.textContent = `${location.localtime.split(" ")[1]}`;    //split date and kept only time
+    iconElement.innerHTML = `<img src="${current.weather_icons[0]}" alt="${current.weather_descriptions[0]}">`;
+    tempElement.innerHTML = `${current.temperature}°C`;
+    descripElement.textContent = `${current.weather_descriptions[0]}`;
+
 }
 
+//Home-Grid function
+function homeGrid(data){
+    //get the required information from AIP/json
+    const current = data.current;
 
+    //creating variables -- for HTML elements
+    const humiElement = document.getElementById("humidity");
+    const windElement = document.getElementById("wind");
+    const visiElement = document.getElementById("visibility");
+    const presElement = document.getElementById("pressure");
+
+    //loading API data into HTML elements
+    humiElement.textContent = `${current.humidity} %`;
+    windElement.textContent = `${current.wind_speed} km/h`;
+    visiElement.textContent = `${current.visibility} km`;
+    presElement.textContent = `${current.pressure} hPa`;
+
+}
+
+Weather();
