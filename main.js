93584323fbe8b0eedfccd3b69bd9bd8bda6b86b0
yogenkarki07 <yogeen399@gmail.com>
2026-08-27 12:41:30 +1200

    Menu completed but over left. Next target search buttton

--- main.js
+++ main.js
@@ -79,3 +79,38 @@ function homeGrid(data){
 }
 
 Weather();
+
+
+
+//menu icon/button 
+const openMenuBtn = document.getElementById("menu");
+const sideMenu = document.getElementById("side-menu");
+const closeMenuBtn = document.getElementById("close-menu");
+const overlay = document.getElementById("overlay");
+
+//open menu
+openMenuBtn.addEventListener("click", function(){
+    
+    console.log("Menu clicked");
+
+    sideMenu.classList.add("active");
+    overlay.classList.add("active");
+})
+
+// close menu using X button
+closeMenuBtn.addEventListener("click", function(){
+
+    console.log("Close button clicked");
+
+    sideMenu.classList.remove("active");
+    sideMenu.classList.remove("active");
+})
+
+//close menu by clicking outside
+closeMenuBtn.addEventListener("click", function(){
+
+    console.log("overlay clicked");
+
+    sideMenu.classList.remove("active");
+    sideMenu.classList.remove("active");
+})
