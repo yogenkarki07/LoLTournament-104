58a3e71ec3d2d119bc9972ca6ff7fcd61134da30
kdowd <kevin.dowd@gmail.com>
2026-09-07 13:31:45 +1200

    add data

--- js/main.js
+++ js/main.js
@@ -5,24 +5,22 @@
 // let userLocation = "Auckland"; // Replace with the desired location
 // const apiURL = "http://api.weatherstack.com/current?access_key=" + apiKey + `&query=${userLocation}`; // Replace 'New York' with the desired location
 
-const apiURL = "./dummy.json"; 
+const apiURL = "./dummy.json";
 
 // const obj = [{},{},{}];
 // async getDefaultWeather(){
 
 // }
 
-
 async function Weather() {
-    try{
-
+  try {
     //fetch the json/api file
-   
-    const response = await fetch (apiURL);
+
+    const response = await fetch(apiURL);
 
     //checking if the request was successful
-    if(!response.ok){
-        throw new Error(`HTTP error! status: ${response.status}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     //convert json/API response into JavaScript object
@@ -33,63 +31,57 @@ async function Weather() {
 
     //data calling from API into grid-boxes elements
     homeGrid(data);
-
-   }
-
-   catch(error){
+  } catch (error) {
     console.error("error fetching weather data:", error);
-   }
-
+  }
 }
 
 //home UI function
-function updatingHomeUI(data){
-    //get the required information from AIP/json
-    const location = data.location;
-    const current = data.current;
+function updatingHomeUI(data) {
+  window.pigg = data;
+  console.log("Updating Home UI with data:", data);
+  //get the required information from AIP/json
+  const location = data.name;
+  const current = data.current;
 
-    // window.mytime = location; <super class="super">&deg;c</super>
+  // window.mytime = location; <super class="super">&deg;c</super>
 
-    //creting variables -- for HTML elements
-    const cityElement = document.getElementById("city");
-    const timeElement = document.getElementById("time");
-    const iconElement = document.getElementById("icon");
-    const tempElement = document.getElementById("temperature");
-    const descripElement = document.getElementById("description");
+  //creting variables -- for HTML elements
+  const cityElement = document.getElementById("city");
+  const timeElement = document.getElementById("time");
+  const iconElement = document.getElementById("icon");
+  const tempElement = document.getElementById("temperature");
+  const descripElement = document.getElementById("description");
 
-    //putting API data into HTML elements
-    cityElement.textContent = `${location.name}`;
-    timeElement.textContent = `${location.localtime.split(" ")[1]}`;    //split date and kept only time
-    iconElement.innerHTML = `<img src="${current.weather_icons[0]}" alt="${current.weather_descriptions[0]}">`;
-    tempElement.innerHTML = `${current.temperature}°C`;
-    descripElement.textContent = `${current.weather_descriptions[0]}`;
-
+  //putting API data into HTML elements
+  cityElement.textContent = `${location.name}`;
+  timeElement.textContent = `${location.localtime.split(" ")[1]}`; //split date and kept only time
+  iconElement.innerHTML = `<img src="${current.weather_icons[0]}" alt="${current.weather_descriptions[0]}">`;
+  tempElement.innerHTML = `${current.temperature}°C`;
+  descripElement.textContent = `${current.weather_descriptions[0]}`;
 }
 
 //Home-Grid function
-function homeGrid(data){
-    //get the required information from AIP/json
-    const current = data.current;
+function homeGrid(data) {
+  //get the required information from AIP/json
+  const current = data.current;
 
-    //creating variables -- for HTML elements
-    const humiElement = document.getElementById("humidity");
-    const windElement = document.getElementById("wind");
-    const visiElement = document.getElementById("visibility");
-    const presElement = document.getElementById("pressure");
+  //creating variables -- for HTML elements
+  const humiElement = document.getElementById("humidity");
+  const windElement = document.getElementById("wind");
+  const visiElement = document.getElementById("visibility");
+  const presElement = document.getElementById("pressure");
 
-    //loading API data into HTML elements
-    humiElement.textContent = `${current.humidity} %`;
-    windElement.textContent = `${current.wind_speed} km/h`;
-    visiElement.textContent = `${current.visibility} km`;
-    presElement.textContent = `${current.pressure} hPa`;
-
+  //loading API data into HTML elements
+  humiElement.textContent = `${current.humidity} %`;
+  windElement.textContent = `${current.wind_speed} km/h`;
+  visiElement.textContent = `${current.visibility} km`;
+  presElement.textContent = `${current.pressure} hPa`;
 }
 
 Weather();
 
-
-
-//menu icon/button 
+//menu icon/button
 const openMenuBtn = document.getElementById("menu");
 const sideMenu = document.getElementById("side-menu");
 const closeMenuBtn = document.getElementById("close-menu");
@@ -98,28 +90,25 @@ const overlay = document.getElementById("overlay");
 console.log("Attaching lister");
 
 //open menu
-openMenuBtn.addEventListener("click", function(){
-    
-    console.log("Menu clicked");
+openMenuBtn.addEventListener("click", function () {
+  console.log("Menu clicked");
 
-    sideMenu.classList.add("active");
-    overlay.classList.add("active");
-})
+  sideMenu.classList.add("active");
+  overlay.classList.add("active");
+});
 
 // close menu using X button
-closeMenuBtn.addEventListener("click", function(){
+closeMenuBtn.addEventListener("click", function () {
+  console.log("Close button clicked");
 
-    console.log("Close button clicked");
-
-    sideMenu.classList.remove("active");
-    overlay.classList.remove("active");
-})
+  sideMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
 
 //close menu by clicking outside
-overlay.addEventListener("click", function(){
+overlay.addEventListener("click", function () {
+  console.log("overlay clicked");
 
-    console.log("overlay clicked");
-
-    sideMenu.classList.remove("active");
-    overlay.classList.remove("active");
-})
+  sideMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
