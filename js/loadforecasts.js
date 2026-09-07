f182339223f371aad2c04b69c4d2c037402653ce
kdowd <kevin.dowd@gmail.com>
2026-09-07 22:06:56 +1200

    added 3 day forcast test page

---
+++ js/loadforecasts.js
@@ -0,0 +1,68 @@
+let cityChoice = document.getElementById('city-choice');
+cityChoice.addEventListener('change', function () {
+    let selectedCity = cityChoice.value;
+    fetchForecast(selectedCity);
+});
+
+function fetchForecast(city) {
+
+    const apiKey = 'e0c93a78a72c46e2bde224929260609';
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3`;
+    //const url = "./forecast.json"; // Use local JSON file for testing
+
+    console.log('API URL:', url);
+    emptyResultsWrapper();
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Forecast data:', data);
+            window.debugData = data; // Store data for debugging
+
+            let daysCount = data.forecast.forecastday.length;
+            createLocationHeader(data.location);
+            createWeatherCard(data.forecast);
+        })
+        .catch(error => {
+            console.error('Error fetching forecast:', error);
+        });
+}
+
+function emptyResultsWrapper() {
+    const container = document.querySelector('.forecasts-wrapper');
+    container.replaceChildren();
+}
+
+function createLocationHeader(locationData) {
+    const container = document.querySelector('.forecasts-wrapper');
+    const template = document.getElementById('location-template');
+    const clone = template.content.cloneNode(true);
+    clone.querySelector(".name").textContent = locationData.name;
+    clone.querySelector(".country").textContent = locationData.country;
+    container.appendChild(clone);
+}
+
+function createWeatherCard(forecastData) {
+
+    const container = document.querySelector('.forecasts-wrapper');
+    const template = document.getElementById('forecast-template');
+    const daysArray = forecastData.forecastday;
+
+    daysArray.forEach(function (e, i) {
+        console.log(e);
+        const clone = template.content.cloneNode(true);
+        clone.querySelector('.forecast-title').textContent = e.date;
+        clone.querySelector('.condition').textContent = e.day.condition["text"];
+        clone.querySelector('.max').innerHTML = e.day.maxtemp_c + "&deg;c";
+        clone.querySelector('.sunrise').textContent = e.astro.sunrise;
+        clone.querySelector('.sunset').textContent = e.astro.sunset;
+
+        container.appendChild(clone);
+
+    });
+
+}
